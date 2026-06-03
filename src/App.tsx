e3fc8ffff91197f9d0ac6a7e4cd930d487094f2c
Antal Arnold Gáti <antal.gati@lateralgroup.com>
2026-06-03 10:52:51 +0300

    Initial React TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <main>
+            <h1>Recipe Manager</h1>
+            <p>Manage your favorite recipes.</p>
+        </main>
+    );
+}
+
+export default App;
