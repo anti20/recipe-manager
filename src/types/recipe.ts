a72cbfac859b4b30c407491cea2cdc4cbda21ca2
Antal Arnold Gáti <antal.gati@lateralgroup.com>
2026-06-03 13:23:07 +0300

    Add recipe data model

---
+++ src/types/recipe.ts
@@ -0,0 +1,27 @@
+export type Recipe = {
+    id: string;
+    title: string;
+    servings: number;
+    cookingTime: number; // in minutes
+    ingredients: Ingredient[];
+    instructions: string[];
+};
+
+export type Ingredient = {
+    name: string;
+    quantity: string;
+    unit: IngredientUnit;
+};
+
+export type IngredientUnit =
+    | "g" // gram
+    | "dkg" // decagram
+    | "kg" // kilogram
+    | "ml" // milliliter
+    | "dl" // deciliter
+    | "l" // liter
+    | "tsp" // teaspoon
+    | "tbsp" // tablespoon
+    | "piece" // piece
+    | "pinch" // pinch
+    | "clove"; // clove
